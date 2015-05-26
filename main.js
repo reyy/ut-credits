071c4b3a8a98355c09392b1a88b355e59af3cfc0
Neo, Rey <rneo@paypal.com>
2015-05-26 10:58:43 +0800

    Added express module

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
-var username = "";
-var password = "";
-
+var express = require('express');
 var Browser = require("zombie");
-var url = "https://myaces.nus.edu.sg/Prjhml/login.do";
-var browser = new Browser();
+var app = express();
 
+
+app.get('/', function (req, res) {
+    var url = "https://myaces.nus.edu.sg/Prjhml/login.do";
+    var browser = new Browser();
+    var username = "hi";
+    var password = "hi";
 
     browser.open();
     browser.visit(url, function(err) {
-        console.log(browser.success);
+        //console.log(browser.success);
         browser
         .fill('input[name="txtUserID"]', username)
         .fill('input[name="txtPwd"]', password)
-        .pressButton('input[value="Login"]', function(res) {
-            console.log(browser.html("body"));
+        .pressButton('input[value="Login"]', function(response) {
+            //console.log(browser.html("body"));
+            var ans = browser.html("body");
+            res.send(ans);
         });
     });
+
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
+
+
+
+    
 
 
