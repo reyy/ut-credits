2cf02105b8b0930d24ad4900a5b421be101f7f4d
Rey Neo <rey@live.jp>
2015-06-22 22:09:11 +0800

    Read in POST variables for user/pass

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var Browser = require("zombie");
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+//Disallow all GET requests
 app.get('/', function (req, res) {
+    return res.sendStatus(400);
+});
+
+app.post('/', function (req, res) {
+    if (!req.body || !req.body.username || !req.body.password) 
+        return res.sendStatus(400);
+
     var url = "https://myaces.nus.edu.sg/Prjhml/login.do";
     var browser = new Browser();
-    var username = "hi";
-    var password = "hi";
+    var username = req.body.username;
+    var password = req.body.password;
 
     browser.open();
     browser.visit(url, function(err) {
@@ -26,16 +37,9 @@ app.get('/', function (req, res) {
 
 var server = app.listen(process.env.PORT || 5000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-
+    console.log('App listening at http://%s:%s', host, port);
 });
-
-
-
-
-    
-
 
