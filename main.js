baa92b324900956e6ab0cdb62da9ae0968dc10c0
Rey Neo <rey@live.jp>
2015-08-21 23:37:09 +0800

    Added set ip/port for openshift

--- main.js
+++ main.js
@@ -93,7 +93,10 @@ app.post('/', function (req, res) {
 
 });
 
-var server = app.listen(process.env.PORT || 5000, function () {
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+var server = app.listen(app.get('port') ,app.get('ip'), function () {
 
     var host = server.address().address;
     var port = server.address().port;
